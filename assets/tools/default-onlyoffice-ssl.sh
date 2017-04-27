28c5599121a01cda1e885a0e5ebaa4edd10523eb
Alexey Bannov <alexey.bannov@onlyoffice.com>
2017-04-27 05:06:17 -0400

    bug-fix

--- assets/tools/default-onlyoffice-ssl.sh
+++ assets/tools/default-onlyoffice-ssl.sh
@@ -7,7 +7,7 @@ cp /app/onlyoffice/config/nginx/onlyoffice-ssl default-onlyoffice-ssl.conf;
 sed 's,{{SSL_CERTIFICATE_PATH}},/var/www/onlyoffice/Data/certs/onlyoffice.crt,' -i default-onlyoffice-ssl.conf;
 sed 's,{{SSL_KEY_PATH}},/var/www/onlyoffice/Data/certs/onlyoffice.key,' -i default-onlyoffice-ssl.conf;
 sed 's,{{SSL_DHPARAM_PATH}},/var/www/onlyoffice/Data/certs/dhparam.pem,' -i default-onlyoffice-ssl.conf;
-sed 's,{{SSL_VERIFY_CLIENT}},false,' -i default-onlyoffice-ssl.conf;
+sed 's,{{SSL_VERIFY_CLIENT}},off,' -i default-onlyoffice-ssl.conf;
 sed '/{{CA_CERTIFICATES_PATH}}/d' -i default-onlyoffice-ssl.conf;
 sed 's/{{ONLYOFFICE_HTTPS_HSTS_MAXAGE}}/31536000/' -i default-onlyoffice-ssl.conf;
 sed 's,{{DOCKER_ONLYOFFICE_SUBNET}},'"${DOCKER_ONLYOFFICE_SUBNET}"',' -i default-onlyoffice-ssl.conf;
