82caded7515bdce89877a59aa1e0bdb4125f5814
Alexey Bannov <alexey.bannov@onlyoffice.com>
2017-05-23 09:29:14 -0400

    fix path

--- assets/tools/letsencrypt.sh
+++ assets/tools/letsencrypt.sh
@@ -6,10 +6,12 @@ for arg; do
     _domains="$_domains -d $arg";
 done
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 certbot certonly --webroot -w /var/www/onlyoffice/Data/certs --noninteractive --agree-tos --email support@$1 $_domains;
 
 ln -sf /etc/letsencrypt/live/$1/fullchain.pem /var/www/onlyoffice/Data/certs/onlyoffice.crt
 ln -sf /etc/letsencrypt/live/$1/privkey.pem /var/www/onlyoffice/Data/certs/onlyoffice.key
 ln -sf /etc/letsencrypt/live/$1/chain.pem /var/www/onlyoffice/Data/certs/stapling.trusted.crt
 
-source default-onlyoffice-ssl.sh
+source $DIR/default-onlyoffice-ssl.sh
